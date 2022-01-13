4f8fa386f1cb87b2763d06a1193e6787235645f4
Adrian John <adrianjohn.developer@gmail.com>
2022-01-13 16:26:46 -0400

    insertion sort completed

--- src/App.js
+++ src/App.js
@@ -15,12 +15,13 @@ function App() {
   useEffect(() => {
     const vw = Math.ceil(Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) / 11)
     if (values.length != vw) {
-      populateArray(vw);
+      populateArray(vw); 
     }
   });
 
   // generates a new random array
   const populateArray = (n) => {
+
     let values = new Array(n);
     for (let i = 0; i < n; i++) {
       values[i] = Math.ceil(Math.random() * 100) + 1
@@ -28,6 +29,12 @@ function App() {
     setValues(values)
   }
 
+  const shuffle = () => {
+
+    let elements = document.getElementsByClassName("bar");
+    for (var i = 0; i < elements.length; i++)  elements[i].style.backgroundColor = "#eee";
+    populateArray(Math.ceil(Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) / 11))
+  }
 
   const sort = async () => {
 
@@ -38,11 +45,13 @@ function App() {
     else if (sort_type === "select")
       console.log(await selection());
 
+    else if (sort_type === "insert")
+      console.log(await insertion());
+
     setDisable(false)
   }
 
-
-  async function bubble() { 
+  async function bubble() {
 
     let values = document.getElementsByClassName("bar");
 
@@ -54,7 +63,7 @@ function App() {
 
         j1.style.background = "#0d6efd";
         j2.style.background = "#0d6efd";
-        await sleep((100.01 - speed)  * 10)
+        await sleep((100.01 - speed) * 10)
 
         let j1_height = parseInt(j1.style.height);
         let j2_height = parseInt(j2.style.height);
@@ -66,13 +75,13 @@ function App() {
           j2.style.height = tmp + "px";
           j1.style.background = "#FB1818";
           j2.style.background = "#FB1818";
-          await sleep((100.01 - speed)  * 10)
-        } 
+          await sleep((100.01 - speed) * 10)
+        }
 
         j1.style.background = "#eee";
         j2.style.background = "#eee";
       }
-      values[ values.length - (i + 1)].style.background = "#74EE66"
+      values[values.length - (i + 1)].style.background = "#74EE66"
     }
 
     return "Bubble Sorted!"
@@ -89,17 +98,17 @@ function App() {
       for (let j = i + 1; j < values.length; j++) {
         values[j].style.background = "#0d6efd";
         let jValue = parseInt(values[j].innerHTML);
-        await sleep((100.01 - speed)  * 10);
+        await sleep((100.01 - speed) * 10);
 
         // update min
         if (jValue < min[0]) {
           values[min[1]].style.background = "#eee";
-          min = [jValue, j]; 
-          values[j].style.background = "#FB1818"; 
-        }else{
+          min = [jValue, j];
+          values[j].style.background = "#FB1818";
+        } else {
           values[j].style.background = "#eee";
         }
-        
+
       }
 
       values[i].style.background = "#eee";
@@ -113,9 +122,49 @@ function App() {
       values[i].style.background = "#74EE66";
     }
 
-    values[values.length -1].style.background = "#74EE66";
+    values[values.length - 1].style.background = "#74EE66";
 
     return "Selection Sort Complete"
+  }
+
+  async function insertion() {
+    let values = document.getElementsByClassName("bar");
+
+   
+
+    for (let i = 0; i < values.length -1; i++) {   
+
+      values[i].style.background = "#74EE66";
+      values[i+1].style.background = "#74EE66";
+
+      if(parseInt(values[i+1].innerHTML) <  parseInt(values[i].innerHTML)){ 
+
+        // loop to find appropriate spot
+        for (let j = i+1; j >= 1; j--) { 
+          
+          values[j].style.background = "red";
+          await sleep((100.01 - speed) * 10);
+          let j0 = parseInt(values[j-1].innerHTML); 
+          let j1 = parseInt(values[j].innerHTML);
+
+          // values[j].style.background = "rgb(255, 165, 0);";
+          // values[j-1].style.background = "rgb(255, 165, 0);";
+  
+          if(j1 < j0){  
+            values [j].innerHTML = j0;
+            values [j].style.height = (j0 * 3)+"px";
+            values[j-1].innerHTML = j1; 
+            values [j-1].style.height = (j1 * 3)+"px"; 
+            values[j].style.background = "#74EE66"; 
+          }
+          else{
+            values[j].style.background = "#74EE66";
+            break;
+          }
+        }
+      }  
+    }
+    return "Insertion sort completed!"
   }
 
   async function sleep(millis) {
@@ -135,10 +184,10 @@ function App() {
           <Button variant="primary" className={sort_type === "quick" ? 'active' : ''} onClick={() => setSortType("quick")}>Quick</Button>{' '}
         </div>
         <div id="slider">
-          <input type="range" id="speed" name="volume" min="1" max="100" defaultValue={speed} onChange={(e) => setSpeed(e.target.value)} disabled={disable}  />
+          <input type="range" id="speed" name="volume" min="1" max="100" defaultValue={speed} onChange={(e) => setSpeed(e.target.value)} disabled={disable} />
           <label htmlFor="volume" id="range_label">Speed</label>
           <Button variant="success" onClick={sort} disabled={disable}  >SORT</Button>{' '}
-          <Button variant="success" onClick={() => window.location.reload()}>SHUFFLE</Button>{' '}
+          <Button variant="success" disabled={disable} onClick={shuffle}>SHUFFLE</Button>{' '}
 
 
         </div>
